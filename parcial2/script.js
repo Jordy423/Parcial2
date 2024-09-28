a3716ca799fabbd9065bd127e1e1c56bcba0e3b7
Jordy423 <133585137+Jordy423@users.noreply.github.com>
2024-09-28 17:02:23 -0600

    Add files via upload

---
+++ parcial2/script.js
@@ -0,0 +1,7 @@
+// Mensaje de bienvenida al cargar la página
+window.onload = function() {
+    alert("¡Bienvenido a nuestra empresa ganadera!");
+};
+
+// Mensaje en la consola
+console.log("Página cargada correctamente");
